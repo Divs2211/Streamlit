9f18d28ca16749b374cf20ef9e8193ad8ff55061
Divs2211 <126938093+Divs2211@users.noreply.github.com>
2023-08-31 22:55:58 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1BnhU01Su8iL7So-fftoPtiSqJ-JpvQbV
 """
 
-!pip install -q streamlit
 import streamlit as st
 import pandas as pd
 from sklearn import datasets
